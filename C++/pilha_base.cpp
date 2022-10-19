8d5b390102c5c66ae3d76423f46ffb04c215282c
LeonidasPedro <leonidas@unochapeco.edu.br>
2022-10-19 19:37:17 -0300

    xurastei

---
+++ C++/pilha_base.cpp
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Estrutura base do nó.
+struct Node
+{
+    int valor;
+    struct Node *proximo;
+};
+
+typedef struct Node node;
+
+void removeElemento(node *raiz);
+void insereElemento(node *raiz, int info);
+void imprimePilha(node *raiz);
+
+int main(void)
+{
+    printf("\n\n\n");
+    node *raiz = NULL;
+    raiz = (node *)malloc(sizeof(node *));
+    raiz->valor = NULL;
+    raiz->proximo = NULL;
+
+    // Mostrando a lista.
+    if (raiz)
+        imprimePilha(raiz);
+
+    int zero = 0;
+    printf("Inserindo novo item: %d\n", zero);
+    insereElemento(raiz, zero);
+    if (raiz)
+        imprimePilha(raiz);
+
+    int um = 1;
+    printf("Inserindo novo item: %d\n", um);
+    insereElemento(raiz, um);
+    if (raiz)
+        imprimePilha(raiz);
+
+    int dois = 2;
+    printf("Inserindo novo item: %d\n", dois);
+    insereElemento(raiz, dois);
+    if (raiz)
+        imprimePilha(raiz);
+
+    printf("Removendo item: %d\n", dois);
+    removeElemento(raiz);
+    if (raiz)
+        imprimePilha(raiz);
+
+    printf("Removendo item: %d\n", um);
+    removeElemento(raiz);
+    if (raiz)
+        imprimePilha(raiz);
+
+    printf("Removendo item: %d\n", zero);
+    removeElemento(raiz);
+    if (raiz)
+        imprimePilha(raiz);
+
+    free(raiz);
+
+    return 0;
+}
+
+void imprimePilha(node *raiz)
+{
+    raiz = raiz->proximo;
+    printf("Mostrando a pilha:\n");
+    if (raiz)
+    {
+        do
+        {
+            printf(" %d", raiz->valor);
+            raiz = raiz->proximo;
+        } while (raiz);
+        printf("\n\n");
+    }
+    else
+        printf("Pilha vazia.\n\n");
+}
---
+++ python/1002.py
@@ -0,0 +1,3 @@
+raio = float(input())
+area = 3.14159 * raio * raio
+print('A={}'.format(area, '.2f'))
