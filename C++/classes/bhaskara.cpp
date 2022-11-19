6c9e22e1787e1ae283c92696514d5f7eeb4178f6
LeonidasPedro <leonidas@unochapeco.edu.br>
2022-11-18 22:39:41 -0300

    bhaskara

---
+++ C++/classes/bhaskara.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+class bhaskara
+{
+private:
+    int a;
+    int b;
+    int c;
+    float r1;
+    float r2;
+    float delta;
+
+public:
+    void setA(int A);
+    void setB(int B);
+    void setC(int C);
+    void setDelta();
+    float getDelta();
+    void setRaizes();
+    float getR1();
+    float getR2();
+};
+
+float bhaskara::getR1() {
+    return r1;
+}
+
+float bhaskara::getR2() {
+    return r2;
+}
+
+void bhaskara::setRaizes(){
+    r1 = sqrt(delta)/(2*a);
+    r2 = -sqrt(delta)/(2*a);
+}
+
+void bhaskara::setA(int A){
+    a = A;
+};
+
+float bhaskara::getDelta(){
+    return delta;
+}
+
+void bhaskara::setB(int B){
+    b = B;
+};
+
+void bhaskara::setC(int C){
+    c = C;
+};
+
+void bhaskara::setDelta(){
+    delta = b * b - 4 * a * c;
+}
+
+int main(){
+
+    bhaskara objeto;
+    float delta;
+
+    objeto.setA(1);
+    objeto.setB(5);
+    objeto.setC(5);
+    objeto.setDelta();
+    objeto.setRaizes();
+
+    delta = objeto.getDelta();
+
+    cout << "Delta: " << delta << endl;
+    cout << fixed << setprecision(2);
+    cout << "r1: " << objeto.getR1() << fixed << setprecision(1) << endl;
+    cout << "r2: " << objeto.getR2() << endl;
+
+    return 0;
+}
--- C++/classes/exemploCalculadora.cpp
+++ C++/classes/exemploCalculadora.cpp
@@ -1,19 +1,41 @@
 #include <stdio.h>
 #include <iostream>
 #include <string>
+
 using namespace std;
 
 class Calculadora {
-   public:
+  private:
     int numero1, numero2;
+
+  public:
     int subtracao();
     int multiplicacao();
-    int soma();
     float divisao();
-
+    int getNumero1();
+    int getNumero2();
+    void setNumero1(int Numero1);
+    void setNumero2(int Numero2);
+    int soma();
 };
 
-int Calculadora::soma() {
+int Calculadora::getNumero1(){
+  return numero1;
+}
+
+int Calculadora::getNumero2(){
+  return numero2;
+}
+
+void Calculadora::setNumero1(int Numero1){
+  numero1 = Numero1;
+}
+
+void Calculadora::setNumero2(int Numero2){
+  numero2 = Numero2;
+}
+
+int Calculadora::soma(){
     return numero1 + numero2;
 }
 
@@ -32,8 +54,9 @@ int Calculadora::multiplicacao() {
 int main() {
     Calculadora calc;
 
-    calc.numero1 = 4;
-    calc.numero2 = 5;
+    calc.setNumero1(1);
+    calc.setNumero2(3);
+
 
     cout << "\nA soma dos números lidos é: " << calc.soma() << "\n\n";
 
