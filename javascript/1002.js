113fe13ac780d1939dd7c442681cea93ef6e28b6
LeonidasPedro <leonidas@unochapeco.edu.br>
2023-02-14 07:04:13 -0300

    hello world ruby

--- javascript/1002.js
+++ javascript/1002.js
@@ -2,19 +2,19 @@ var input = require('fs').readFileSync('/dev/stdin', 'utf8');
 var lines = input.split('\n');
 
 /*
-#include <stdio.h>
- 
-int main() {
- 
-    double A, R;
-    scanf("%lf", &R);
-    A = 3.14159 * R * R;
-    printf("A=%.4lf", A);
-    printf("\n");
-   
-    return 0;
-}
- */
+// #include <stdio.h>
+//  
+// int main() {
+//  
+//     double A, R;
+//     scanf("%lf", &R);
+//     A = 3.14159 * R * R;
+//     printf("A=%.4lf", A);
+//     printf("\n");
+//    
+//     return 0;
+// }
+//  */
 
 let A 
 let R = Number(lines.shift());
--- ruby/entradasNum.rb
+++ ruby/entradasNum.rb
@@ -8,3 +8,7 @@ print "Digite o segundo número inteiro: "
 numero2 = gets.chomp.to_i##chomp retira o caractere de quebra de linha 
 
 puts numero1 + numero2
+
+array = [1, 2, 3]
+puts array.class ##tipo da variavel
+
