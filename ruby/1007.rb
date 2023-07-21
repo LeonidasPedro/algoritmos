6b9b26b878e86c03befb359451a1b7a7c6527eb8
leonidaspedro <leonidas@unochapeco.edu.br>
2023-07-21 16:41:41 -0300

    rb

---
+++ ruby/1007.rb
@@ -0,0 +1,11 @@
+# Leia quatro valores inteiros A, B, C e D. A seguir, calcule e mostre a diferença
+# do produto de A e B pelo produtode C e D segundo a fórmula: DIFERENCA = (A * B - C * D).
+
+a = gets.to_i
+b = gets.to_i
+c = gets.to_i
+d = gets.to_i
+
+difference = (a * b - c * d)
+
+puts "DIFERENCA = #{difference}"
